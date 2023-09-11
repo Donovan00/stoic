9b147e76b34d743dfe17ea3191e383d15ec6f69b
Donovan Dooley <donovandooley00@gmail.com>
2023-09-10 23:43:22 -0700

    working css styles

--- src/Pages/BookPage.js
+++ src/Pages/BookPage.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import NavigationBar from '../NavigationBar';
-import './BookPage.module.css'; 
-
+import styles from './BookPage.module.css'; // Import the module.css as 'styles'
 
 function BookPage() {
   return (
-    <div className="App">
+    <div className={styles.App}>
       <NavigationBar />
-      <header className="App-header">
+      <header className={styles['App-header']}>
         <h1>Books on Stoicism</h1>
       </header>
-      <ul className="book-list">
-        <li className="book-list-item">The Art of Living</li>
-        <li className="book-list-item">Meditations</li>
-        <li className="book-list-item">Letters from a Stoic</li>
-        <li className="book-list-item">The Obstacle Is the Way</li>
-        <li className="book-list-item">Stillness Is The Key</li>
+      <ul className={styles['book-list']}>
+        <li className={styles['book-list-item']}>The Art of Living</li>
+        <li className={styles['book-list-item']}>Meditations</li>
+        <li className={styles['book-list-item']}>Letters from a Stoic</li>
+        <li className={styles['book-list-item']}>The Obstacle Is the Way</li>
+        <li className={styles['book-list-item']}>Stillness Is The Key</li>
       </ul>
     </div>
   );
